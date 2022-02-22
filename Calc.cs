690fa341cb686bc71a0566f6ba2232455636c34d
zzijad <zzzijo@gmail.com>
2022-02-22 14:50:00 +0100

    Add project files.

---
+++ Calc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    public class Calc
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Rezultat { get; set; }
+
+        public void Sabiranje()
+        {
+            Rezultat = X + Y;
+        }
+
+        public void Oduzimanje()
+        {
+            Rezultat = X - Y;
+        }
+
+        public void Mnozenje()
+        { 
+            Rezultat = X * Y;
+        }
+
+        public void Deljenje()
+        { 
+            Rezultat = X / Y;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kalkulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new Calc();
+        }
+
+        //TODO Izmeniti klasu Calc, tako da ima svih 5 operacija (+,-,*,/, modulus :P )
+        //ALIIIIIIIIIIIIIIIIIIIIII
+        //Svih pet mora da se rade iz iste metode
+        //tako sto ce ta medtoda imati string kao ulazni parametar
+        //i switch case ili if da zna sta treba da radi :)
+
+        private void Sabiranje(object sender, RoutedEventArgs zklj)
+        {
+            if (DataContext is Calc c)
+            {
+                c.Sabiranje();
+                MessageBox.Show(c.Rezultat.ToString());
+            }
+        }
+
+        private void Oduzimanje(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is Calc c)
+            {
+                c.Oduzimanje();
+                MessageBox.Show(c.Rezultat.ToString());
+            }
+        }
+
+        private void Mnozenje(object sender, RoutedEventArgs mzlj)
+        {
+            if (DataContext is Calc c)
+            { 
+                c.Mnozenje();
+                MessageBox.Show(c.Rezultat.ToString());
+            }
+        }
+
+        private void Deljenje(object sender, RoutedEventArgs dlj)
+        {
+            if (DataContext is Calc c)
+            { 
+                c.Deljenje();
+                MessageBox.Show(c.Rezultat.ToString());
+            }
+        }
+    }
+}
